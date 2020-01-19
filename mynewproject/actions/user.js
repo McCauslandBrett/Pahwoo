aa2b42e7cfc26303cfa34484f8e0e3b76e11bac5
Rogelio Macedo <rmace001@ucr.edu>
2020-01-19 13:05:59 -0800

    Add user actions closes #3

---
+++ mynewproject/actions/user.js
@@ -0,0 +1,12 @@
+export const updateEmail = (email) => {
+  return {type:'UPDATE_EMAIL', payload:email}
+}
+export const updatePassword = (password) => {
+  return {type:'UPDATE_PASSWORD',payload:password}
+}
+export const updateUsername = (username) => {
+  return {type:'UPDATE_USERNAME',payload:username}
+}
+export const updateBirthday = (birthday) => {
+  return {type:'UPDATE_BIRTHDAY', payload:birthday}
+}
