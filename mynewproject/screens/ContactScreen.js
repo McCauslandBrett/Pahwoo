43f732a5519abb231ea14ef18c867e17c2d5b306
Rogelio Macedo <rmace001@ucr.edu>
2020-01-31 16:48:23 -0800

    Update styling of contacts and display list dynamically

--- mynewproject/screens/ContactScreen.js
+++ mynewproject/screens/ContactScreen.js
@@ -1,9 +1,26 @@
 import React, {Component} from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, SectionList} from 'react-native';
 import {Container,Header, Left, Right, Content, List, ListItem } from 'native-base';
 import { connect } from 'react-redux';
 import {bindActionCreators} from 'redux';
 import Icon  from "../components/icons.js"
+
+
+// might want to fetch the user's contacts in the ComponentDidMount function
+// that way they are loaded when the ContactScreen Component loads
+// What features/functionality should the contact screen have? 
+// What should the contact screen look like?
+
+
+
+
+function Item({ title }) {
+    return (
+      <View style={styles.item}>
+        <Text style={styles.title}>{title}</Text>
+      </View>
+    );
+}
 
 class ContactScreen extends Component{
   static navigationOptions = {
@@ -11,49 +28,111 @@ class ContactScreen extends Component{
       <Icon.FontAwesome name= "address-book" style = {{fontSize:24, color:tintColor}}/>
     )
   }
+  
+  componentDidMount(){
+      this.loadContacts()
+  }
+//   contactData = []
+  contactData = [
+    {
+        title: 'A', 
+        data: ['Ben Aberdeen', 'Range Alexis', 'Covalt Azure']
+    },
 
+    {
+        title: 'B', 
+        data: ['Tommy Benson', 'Billy Blanks', 'Andy Bolton']
+    },
+
+    {
+        title: 'C', 
+        data: ['Thomas Cat', 'Cassidy Christopher', 'CC Cephala']
+    },
+
+    {
+        title: 'D', 
+        data: ['Dan Daily', 'Guy Dorman', 'Gigi Dozophere']
+    }
+  ]
+  loadContacts(){
+    // initialize data
+    this.contactData = []
+    this.contactData.push(
+        {
+            title: 'A', 
+            data: ['Ben Aberdeen', 'Range Alexis', 'Covalt Azure']
+        }
+    )
+    this.contactData.push(
+        {
+            title: 'B', 
+            data: ['Tommy Benson', 'Billy Blanks', 'Andy Bolton']
+        }
+    )
+    this.contactData.push(
+        {
+            title: 'C', 
+            data: ['Thomas Cat', 'Cassidy Christopher', 'CC Cephala']
+        }
+    )
+    this.contactData.push(
+        {
+            title: 'D', 
+            data: ['Dan Daily', 'Guy Dorman', 'Gigi Dozophere']
+        }
+    )
+    // More things needed here
+  }
+  
     render() {
       return (
-
         <Container>
         <View>
-          <Header>
-           <Left>
-             <Icon.FontAwesome name = "bars" size ={24} onPress={ () => this.props.navigation.openDrawer()}   />
-          </Left>
-          </Header>
+        <Header>
+            <Left>
+                <Icon.FontAwesome name = "bars" size ={24} onPress={ () => this.props.navigation.openDrawer()}   />
+            </Left>
+        </Header>   
         </View>
-          <Content>
-            <List>
-              <ListItem itemDivider>
-                <Text>A</Text>
-              </ListItem>
-              <ListItem>
-                <Text>Aaron Bennet</Text>
-              </ListItem>
-              <ListItem>
-                <Text>Ali Connors</Text>
-              </ListItem>
-              <ListItem itemDivider>
-                <Text>B</Text>
-              </ListItem>
-              <ListItem>
-                <Text>Bradley Horowitz</Text>
-              </ListItem>
-            </List>
-          </Content>
+        <SafeAreaView style={styles.container}>
+        <SectionList
+            sections={this.contactData}
+            keyExtractor={(item, index) => item + index}
+            renderItem={({ item }) => <Item title={item} />}
+            renderSectionHeader={({ section: { title } }) => (
+            <Text style={styles.header}>{title}</Text>
+            )}
+        />
+        
+        </SafeAreaView>
         </Container>
       );
     }
   }
 
-const syles = StyleSheet.create({
+const styles = StyleSheet.create({
   container:{
     flex:1,
     alignItems:'center',
-    justifyContent:'center'
+    justifyContent:'center',
+    alignSelf: 'stretch',
+    flexDirection: 'row'
+  },
+  item: {
+    backgroundColor: 'lightgreen',
+    padding: 20,
+    marginVertical: 8,
+    justifyContent: 'center'
+  },
+  header: {
+    fontSize: 32,
+  },
+  title: {
+    fontSize: 24,
   }
 });
+
+
 const mapDispatchToProps = (dispatch) => {
   return bindActionCreators({},dispatch)
 }
