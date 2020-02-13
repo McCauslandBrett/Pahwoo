f53b0ccb2d47936c1fa4f9bcbdae163c20dcf987
Rogelio Macedo <rmace001@ucr.edu>
2020-02-12 23:09:50 -0800

    Populate user's savedTemplates array on card creation

--- mynewproject/actions/card.js
+++ mynewproject/actions/card.js
@@ -4,12 +4,20 @@ import db from '../config/firebase';
 export const createCard = (newName) => {
 	return async (dispatch, getState) => {
 		try {
-			const card = {
+            const { post, user } = getState()
+            const card = {
 				name: newName
-			}
+            }
+            // Add card to database
 			const ref = db.collection('cards').doc()
 			card.id = ref.id
-			await ref.set(card)
+            await ref.set(card)
+            
+            //Add card to user.savedTemplates[]
+            var userRef = db.collection('users').doc(user.uid)
+            await userRef.update({
+                savedTemplates: firebase.firestore.FieldValue.arrayUnion(card.id)
+            });
 		} catch (e) {
 			alert(e)
 		}
