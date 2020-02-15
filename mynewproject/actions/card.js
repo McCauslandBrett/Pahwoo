d34dce42342f8c61fdfab666a6ab805e7d757f6a
Rogelio Macedo <rmace001@ucr.edu>
2020-02-14 22:45:53 -0800

    Reference Card ID and add users to card closes #32

--- mynewproject/actions/card.js
+++ mynewproject/actions/card.js
@@ -1,12 +1,47 @@
 import firebase from 'firebase';
 import db from '../config/firebase';
 import uuid from 'uuid'
-export const createCard = (newName) => {
+export const createCard = (newName, recipientsList) => {
+	return async (dispatch, getState) => {
+		try {
+            const { user } = getState()
+            const cardObject = {
+                name: newName, 
+                recipients: recipientsList,
+                cover_text: '',
+                body_one_text: '',
+                body_two_text: '',
+                cover_font: '',
+                cover_text_align: '',
+                cover_text_bold: '',
+                cover_text_italic: ''
+            }
+            // Add card to database
+			const ref = db.collection('cards').doc()
+			cardObject.id = ref.id
+            await ref.set(cardObject)
+            // we should save the card id here to the card state!!!!!!!
+            //Add card to user.savedTemplates[]
+            var userRef = db.collection('users').doc(user.uid)
+            await userRef.update({
+                savedTemplates: firebase.firestore.FieldValue.arrayUnion(cardObject.id)
+            });
+            dispatch( {type:'SET_CID',payload: cardObject.id})
+		} catch (e) {
+			alert(e)
+		}
+	}
+}
+
+export const sendCard = (recipients) => {
+    // just a copy paste from createCard, but should be modified to iterate
+    // over the cards recipients and populate their recievedCards array
 	return async (dispatch, getState) => {
 		try {
             const { post, user } = getState()
             const card = {
-				name: newName
+                name: newName, 
+                recipients: recipientsList
             }
             // Add card to database
 			const ref = db.collection('cards').doc()
@@ -15,14 +50,16 @@ export const createCard = (newName) => {
 
             //Add card to user.savedTemplates[]
             var userRef = db.collection('users').doc(user.uid)
+            // for loop
             await userRef.update({
-                savedTemplates: firebase.firestore.FieldValue.arrayUnion(card.id)
+                recievedCards: firebase.firestore.FieldValue.arrayUnion(card.id)
             });
 		} catch (e) {
 			alert(e)
 		}
 	}
 }
+
 export const updateCoverText = (text) => {
   return {type:'UPDATE_COVER_TEXT', payload:text}
 }
--- mynewproject/screens/TemplateCardScreen.js
+++ mynewproject/screens/TemplateCardScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Modal, TextInput} from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Modal, SafeAreaView, TextInput} from 'react-native';
 import {Header, Left, Right} from 'native-base';
 import { connect } from 'react-redux';
 import {bindActionCreators} from 'redux';
@@ -7,6 +7,7 @@ import Icon  from "../components/icons.js";
 import styles from '../styles.js'
 import db from '../config/firebase.js';
 import {createCard} from '../actions/card.js'
+import MultiSelect from 'react-native-multiple-select';
 class TemplateCardScreen extends Component{
   static navigationOptions = {
     drawerIcon : ({tintColor}) => (
@@ -15,16 +16,60 @@ class TemplateCardScreen extends Component{
   }
   state = {
     modalVisible: false,
-    newName: ``
+    selectContactsVisible: false,
+    newName: ``,
+    selectedItems: [],
+    contactData: []
   }
   setModalVisible = (visible) => {
     this.setState({modalVisible: visible});
   }
-  createCard = () => {
+  setSelectContactsModalVisible = (visible) => {
+    this.setState({selectContactsVisible: visible});
+  }
+  createCard = async () => {
    //   should create a card with just a name for now and add it to the database
     console.log(this.state.newName)
-    this.props.createCard(this.state.newName)
+    console.log(this.state.selectedItems)
+    // why should the next line have `await`? Bcus we log props.card.cid right after and want that to be set
+    await this.props.createCard(this.state.newName, this.state.selectedItems)
+    console.log(this.props.card.cid)
   }
+  sendCard = () => {
+    //   should create a card with just a name for now and add it to the database
+     this.props.sendCard(this.state.selectedItems)
+   }
+  onSelectedItemsChange = selectedItems => {
+    this.setState({selectedItems: selectedItems});
+    console.log(this.state.selectedItems)
+  };
+  componentDidMount = async () => {
+    let tempData = []
+    // for loop with async calls
+    for (var i = 0; i < this.props.user.contacts.length; i++){
+        const query = await db.collection('users').where('uid', '==', this.props.user.contacts[i]).get()
+        query.forEach((response) => {
+            tempData.push(response.data())
+        })
+    }
+    
+    let mappedData = tempData.map(item => {
+        // add these to fields to every item in tempData
+        item.isSelect = false;
+        item.selectedClass = styles.list;
+        
+        return item;
+    })
+    console.log('begin ROGITH log')
+    console.log(mappedData)
+    console.log('EEEEEEEEND ROGITH log')
+    // at this point I have an array of user objects from the current user's contact array
+    
+    // tempData.sort((a, b) => (a.username > b.username) ? 1 : -1)
+    mappedData.sort((a, b) => (a.username > b.username) ? 1 : -1)
+    // this.setState({contactData: tempData})
+    this.setState({contactData: mappedData})
+}
 
   render(){
     return(
@@ -48,6 +93,46 @@ class TemplateCardScreen extends Component{
             }}>
             <Text>Create</Text>
         </TouchableOpacity>
+        <TouchableOpacity style={styles.button}
+            onPress={() => {this.setSelectContactsModalVisible(true)}}>
+            <Text>Select Contacts</Text>
+        </TouchableOpacity>
+        <Modal
+                animationType="slide"
+                transparent={false}
+                visible={this.state.selectContactsVisible}
+                onRequestClose={() => {
+                alert('Modal has been closed.');
+            }}>
+                <SafeAreaView style={{flex: 1}}>
+                    <MultiSelect
+                        hideTags
+                        items={this.state.contactData}
+                        uniqueKey="uid"
+                        // ref={(component) => { this.state.multiSelect = component }}
+                        onSelectedItemsChange={this.onSelectedItemsChange}
+                        selectedItems={this.state.selectedItems}
+                        selectText="Pick Items"
+                        searchInputPlaceholderText="Search Items..."
+                        tagRemoveIconColor="#CCC"
+                        tagBorderColor="#CCC"
+                        tagTextColor="#CCC"
+                        selectedItemTextColor="#CCC"
+                        selectedItemIconColor="#CCC"
+                        itemTextColor="#000"
+                        displayKey="username"
+                        searchInputStyle={{ color: '#CCC' }}
+                        submitButtonColor="#CCC"
+                        submitButtonText="Submit"
+                    />
+                </SafeAreaView>
+                <TouchableOpacity style={styles.button}
+                    onPress={() => {
+                    this.setSelectContactsModalVisible(!this.state.selectContactsVisible);
+                    }}>
+                    <Text>Hide Modal</Text>
+                </TouchableOpacity>
+            </Modal>
 
         <TouchableOpacity style={styles.button}
             onPress={() => {
