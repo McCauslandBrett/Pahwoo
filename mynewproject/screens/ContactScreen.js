00cddd88811ead9d957e0dcf3c87f81c7fa28b16
Rogelio Macedo <rmace001@ucr.edu>
2020-03-06 03:14:07 -0800

    Complete selecting contact to view profile screen

--- mynewproject/screens/ContactScreen.js
+++ mynewproject/screens/ContactScreen.js
@@ -7,6 +7,8 @@ import {Container,Header, Left} from 'native-base';
 import { connect } from 'react-redux';
 import {bindActionCreators} from 'redux';
 import db from '../config/firebase.js';
+import {getContactProfile} from '../actions/user.js';
+
 import {FriendModal} from '../components/FriendModal.js'
 class ContactScreen extends Component{
     static navigationOptions = {
@@ -17,6 +19,9 @@ class ContactScreen extends Component{
 
     state = {
         contactData: [],
+        modalVisible: false,
+        modalProfileImage: '',
+        modalProfileName: ''
     }
 
     componentDidMount = async () => {
@@ -33,9 +38,30 @@ class ContactScreen extends Component{
 
         tempData.sort((a, b) => (a.username > b.username) ? 1 : -1)
         this.setState({contactData: tempData})
+        this.props.getContactProfile(false);
+    }
+    
+    onSelect = (profileImage, profileName) => {
+        // this.setState({modalVisible: true});
+        // await this.props.getCard(id)
+        // this.setState({cardID: this.props.card.id})
+        // this.setSelectCardModalVisible(true);
+        this.setState({modalProfileImage: profileImage});
+        this.setState({modalProfileName: profileName});
+        this.props.getContactProfile(true);
+        
+    }
+    
+    displayModal() {
+        if (this.props.user.selectedProfileView){
+            return <FriendModal p={this.state.modalProfileImage} o={true} props={this.props} username={this.state.modalProfileName}/>;
+        } else {
+            return <View></View>;
+        }
     }
 
     render() {
+        
       return (
         <Container>
             <View>
@@ -45,6 +71,7 @@ class ContactScreen extends Component{
                     </Left>
                 </Header>
             </View>
+            <View>{this.displayModal()}</View>
             <SafeAreaView style={styles.contactsContainer}>
                 <FlatList
                     data={this.state.contactData}
@@ -55,6 +82,7 @@ class ContactScreen extends Component{
                             username={item.username}
                             profileImage={item.profileImage}
                             userContacts={this.state.contactData}
+                            onSelect={this.onSelect}
                         />
                     )}
                     keyExtractor={item => item.uid}
@@ -69,16 +97,13 @@ class ContactScreen extends Component{
     }
 }
 
-function Item({ uid, birthday, username, profileImage, userContacts}) {
+
+
+function Item({ uid, birthday, username, profileImage, userContacts, onSelect}) {
     return (
         <TouchableOpacity
             style={styles.list}
-            onPress={() => {
-              <FriendModal
-                p={profileImage}
-                o={true}
-              />
-            }}
+            onPress={() => onSelect(profileImage, username)}
         >
             <Image
                 source={{ uri: profileImage }}
@@ -91,7 +116,7 @@ function Item({ uid, birthday, username, profileImage, userContacts}) {
 }
 
 const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({},dispatch)
+  return bindActionCreators({getContactProfile},dispatch)
 }
 const mapStateToProps = (state) => {
   return {
