dae343725c4e3ae03e39c27d210442eaa808c9b2
Brett McCausland <brettmccausland@gmail.com>
2020-04-02 16:58:34 -0700

    working accordian

--- mynewproject/screens/SettingScreen.js
+++ mynewproject/screens/SettingScreen.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import { View, Text, StyleSheet, Button,SafeAreaView,ScrollView} from 'react-native';
+import { View, Text, StyleSheet, Button,SafeAreaView,
+  ScrollView,TextInput} from 'react-native';
 import {Header, Left, Right} from 'native-base';
-
+import { List, Checkbox } from 'react-native-paper';
 import Icon  from "../components/icons.js";
 import {Ionicons} from "@expo/vector-icons";
 
@@ -10,6 +11,7 @@ import {connect} from 'react-redux'
 import {logout} from '../actions/user.js'
 import firebase from 'firebase';
 import styles from '../styles.js'
+
 class SettingScreen extends Component{
 
   static navigationOptions = {
@@ -17,6 +19,14 @@ class SettingScreen extends Component{
        <Icon.FontAwesome name= "cogs" style = {{fontSize:24, color:tintColor}}/>
     )
   }
+  state = {
+  expanded: true
+};
+
+_handlePress = () =>
+  this.setState({
+    expanded: !this.state.expanded
+  });
 
   logout = () => {
     firebase.auth().signOut()
@@ -25,6 +35,8 @@ class SettingScreen extends Component{
   }
 
   render(){
+
+
     return(
       <SafeAreaView >
    <ScrollView showsVerticalScrollIndicator={false}>
@@ -34,8 +46,43 @@ class SettingScreen extends Component{
 
 
       <View style = {{marginTop:60, alignItems:"center"}}>
-          <Text> SettingScreen</Text>
-          <Button title ="logout" onPress = {()=> this.logout()} />
+
+
+          <View   style = {styles.border}>
+            <Button title ="logout" onPress = {()=> this.logout()} />
+          </View>
+          <List.Section title="Accordions">
+              <List.Accordion
+                title="Uncontrolled Accordion"
+                left={props => <List.Icon {...props} icon="folder" />}
+              >
+              <List.Item title="First item" />
+              <List.Item title="Second item" />
+          </List.Accordion>
+
+        <List.Accordion
+          title="Controlled Accordion"
+          left={props => <List.Icon {...props} icon="folder" />}
+          expanded={this.state.expanded}
+          onPress={this._handlePress}
+        >
+          <List.Item title="First item" />
+          <List.Item title="Second item" />
+        </List.Accordion>
+      </List.Section>
+
+          <View   style = {styles.border}>
+            <Text>Hello</Text>
+          </View>
+
+          <View   style = {styles.border}>
+            <Text>Hello</Text>
+          </View>
+
+          <View   style = {styles.border}>
+            <Text>Hello</Text>
+          </View>
+
       </View>
       </ScrollView>
   </SafeAreaView>
