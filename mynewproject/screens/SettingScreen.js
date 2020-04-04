6d733db5fa1cc32acd69e0c4cb02a4461a8a960d
Brett McCausland <brettmccausland@gmail.com>
2020-04-03 21:41:16 -0700

    menu in settings laidout still need implement acordian

--- mynewproject/screens/SettingScreen.js
+++ mynewproject/screens/SettingScreen.js
@@ -1,17 +1,20 @@
 import React, {Component} from 'react';
 import { View, Text, StyleSheet, Button,SafeAreaView,
-  ScrollView,TextInput} from 'react-native';
+  ScrollView,TextInput,TouchableOpacity} from 'react-native';
 import {Header, Left, Right} from 'native-base';
 import { List, Checkbox } from 'react-native-paper';
 import Icon  from "../components/icons.js";
-import {Ionicons} from "@expo/vector-icons";
+
+import {Ionicons,Entypo,
+        MaterialCommunityIcons,
+        SimpleLineIcons,AntDesign} from "@expo/vector-icons";
 import {Collapse,CollapseHeader, CollapseBody, AccordionList} from 'accordion-collapse-react-native';
 import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 import {logout} from '../actions/user.js'
 import firebase from 'firebase';
 import styles from '../styles.js'
-
+import { Chevron } from 'react-native-shapes';
 class SettingScreen extends Component{
 
   static navigationOptions = {
@@ -36,57 +39,110 @@ _handlePress = () =>
 
   render(){
 
-
+    const optionContainer = [localstyles.rowcontainer]
     return(
       <SafeAreaView >
    <ScrollView showsVerticalScrollIndicator={false}>
 
           <Icon.FontAwesome name = "bars" style = {styles.menuIcon} size ={24} onPress={ () => this.props.navigation.openDrawer()}   />
-          <Ionicons name="md-more" size={24} style = {styles.mdmore} />
+          
           <View style = {{marginBottom:50}} />
 
           <Collapse>
             <CollapseHeader>
-              <View>
-                <Text>Click here</Text>
+              <View style = {localstyles.rowcontainer}>
+                  <MaterialCommunityIcons name= "palette-advanced" style = {localstyles.optionlogo}/>
+                  <Text style = {localstyles.optiontextTitle}>Theme Options </Text>
+                  <Icon.AntDesign style = {localstyles.optiontoggle} size={24} name= "down" color="gray" />
               </View>
             </CollapseHeader>
-            <CollapseBody>
-              <Text>Ta daa!</Text>
-              <Text>Ta daa!</Text>
-              <Text>Ta daa!</Text>
+            <CollapseBody style = {localstyles.container}>
+
+             <View style = {localstyles.optionContainer}>
+              <Text style = {localstyles.option}> light Mode </Text>
+             </View>
+
+             <View style = {localstyles.optionContainer}>
+              <Text style = {localstyles.option}> Dark Mode </Text>
+             </View>
+
+              <View style = {localstyles.optionContainer}>
+                <Text style = {localstyles.option}> Pahwoo </Text>
+              </View>
+
             </CollapseBody>
           </Collapse>
 
+          <TouchableOpacity onPress = {()=> this.logout()} style = {localstyles.rowcontainer}>
+            <SimpleLineIcons name= "logout" style = {localstyles.optionlogo}/>
+            <Text style = {localstyles.optiontextTitle}>logout </Text>
+          </TouchableOpacity>
 
-      <View style = {{marginTop:60, alignItems:"center"}}>
+          <TouchableOpacity  style = {localstyles.rowcontainer}>
+            <AntDesign name= "edit" style = {localstyles.optionlogo}/>
+            <Text style = {localstyles.optiontextTitle}>Username </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity  style = {localstyles.rowcontainer}>
+            <AntDesign name= "edit" style = {localstyles.optionlogo}/>
+            <Text style = {localstyles.optiontextTitle}>Email </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity  style = {localstyles.rowcontainer}>
+            <AntDesign name= "edit" style = {localstyles.optionlogo}/>
+            <Text style = {localstyles.optiontextTitle}>Password </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity  style = {localstyles.rowcontainer}>
+            <MaterialCommunityIcons name= "calendar-edit" style = {localstyles.optionlogo}/>
+            <Text style = {localstyles.optiontextTitle}>Birthday </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity  style = {localstyles.rowcontainer}>
+            <MaterialCommunityIcons name= "credit-card-multiple" style = {localstyles.optionlogo}/>
+            <Text style = {localstyles.optiontextTitle}>Payment </Text>
+          </TouchableOpacity>
 
 
-          <View   style = {styles.border}>
-            <Button title ="logout" onPress = {()=> this.logout()} />
-          </View>
 
-
-          <View   style = {styles.border}>
-            <Text>Hello</Text>
-          </View>
-
-          <View   style = {styles.border}>
-            <Text>Hello</Text>
-          </View>
-
-          <View   style = {styles.border}>
-            <Text>Hello</Text>
-          </View>
-
-      </View>
       </ScrollView>
   </SafeAreaView>
     );
   }
 }
 
+const localstyles = StyleSheet.create({
+  optionlogo:{
+    fontSize:30,
+  },
+  rowcontainer:{
+    marginTop:15,
+    marginLeft:15,
+    flexDirection: 'row',
+    alignItems:'center',
 
+  },
+  optiontoggle:{
+    marginRight:10,
+
+  },
+  optionContainer:{
+    paddingVertical:10
+  },
+  container:{
+    marginTop:15,
+    marginLeft:15,
+    paddingHorizontal:10
+  },
+  optiontextTitle:{
+    fontSize:20,
+    paddingHorizontal:10,
+    marginLeft:20,
+  },
+  option:{
+    fontSize:20
+  }
+});
 const mapDispatchToProps = (dispatch) => {
     return bindActionCreators({logout},dispatch)
   }
